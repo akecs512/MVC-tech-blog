353161029dddc0d955aef7497c40556664912b32
Adrienne Kecskes <akecs619@yahoo.com>
2023-09-01 18:11:48 -0500

    content populates on blogs page when clicked

---
+++ controllers/api/blogs.js
@@ -0,0 +1,12 @@
+const { TechInfo } = require('../../models');
+
+const router = require('express').Router();
+
+router.get('/:id', async (req, res) => {
+    const blog = await TechInfo.findOne({ where: { id: req.params.id } });
+    console.log(blog);
+    res.render('blogs', {blog: blog.get({ plain: true })});
+    
+  });
+  
+  module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const techRoutes = require('./tech_info_routes');
 const userRoutes = require('./user_routes');
 const pageRoutes = require('./pages_route');
+const blogRoutes = require('./blogs');
 
 
 router.use('/tech', techRoutes);
 router.use('/user', userRoutes);
 router.use('/pages', pageRoutes);
+router.use('/blogs', blogRoutes);
 
 module.exports = router;

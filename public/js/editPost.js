5f0045343e815c98f2ea3f134172f63a82eda425
Adrienne Kecskes <akecs619@yahoo.com>
2023-09-18 13:10:31 -0500

    update post button

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,28 +1,30 @@
 async function newFormHandler(event) {
-  event.preventDefault();
-  const text = document.querySelector("#comment-text").value.trim();
-  const postId = document.querySelector("#post-id").value;
-
-  const response = await fetch(`/api/posts`, {
-    method: "PUT",
-    body: JSON.stringify({ text, postId }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  if (response.ok) {
-    window.location.replace("/posts/" + postId);
-  } else {
-    alert("Failed to add comment");
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const editButtons = document.querySelectorAll(".edit-button");
-  editButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-    //   const id = e.target.getAttribute("data-id");
-    //   window.location.replace("/edit/" + id);
+    event.preventDefault();
+    const postTitle = document.querySelector("#post-title").value;
+    const postDescription = document
+      .querySelector("#post-description")
+      .value.trim();
+      const postId = document.querySelector("#post-id").value;
+  
+    let response = await fetch(`/api/posts/${postId}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        title: postTitle,
+        description: postDescription,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
-  });
-});
+    if (response.ok) {
+      window.location.replace("/dashboard");
+    } else {
+      alert("Failed to add post post");
+    }
+  }
+  
+  const form = document.querySelector("#new-post-form");
+  if (form) {
+    form.addEventListener("submit", newFormHandler);
+  }
+  
